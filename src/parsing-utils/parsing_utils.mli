8c1d075d4ea0fd94ea15902580f9369d45eb9bef
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-08-17 22:18:16 +0200

    Add parsing-utils

---
+++ src/parsing-utils/parsing_utils.mli
@@ -0,0 +1,71 @@
+(*_********************************************************************************)
+(*_  parsing-utils: Making it easier to use generated Parsers/Lexers              *)
+(*_  SPDX-FileCopyrightText: 2023-2026 Mathieu Barbin <mathieu.barbin@gmail.com>  *)
+(*_  SPDX-License-Identifier: MIT                                                 *)
+(*_********************************************************************************)
+
+(** This modules implements utils to call parsing functions given a Parser/Lexer
+    pair. The pattern here is for a library to implement the [S] interface,
+    and then use the functions provided here by supplying [S] as a first class
+    module. For example:
+
+    {v
+      let result =
+        Parsing_utils.parse_file
+          (module Parser)
+          ~path
+      in
+      ...
+    v}
+
+    There are several styles offered depending on the context:
+
+    1. Using the [Parsing_result] type.
+    2. Using [Err].
+
+    In all cases, the functions take care of producing located error messages
+    containing the name of the file and the position of the syntax error if any.
+
+    The functions below that do not read the contents from a file still require
+    a path to be provided, which will be used for error messages only (example
+    when parsing the contents from stdin or a string). *)
+
+module type S = sig
+  type token
+  type t
+
+  val lexer : Lexing.lexbuf -> token
+  val parser : (Lexing.lexbuf -> token) -> Lexing.lexbuf -> t
+end
+
+module Parsing_result : sig
+  type error =
+    { loc : Loc.t
+    ; exn : exn
+    }
+
+  type 'a t = ('a, error) Result.t
+
+  (** [ok_exn r] is [x] when r is [Ok x]. Otherwise it raises an [Err.E]
+      exception. *)
+  val ok_exn : 'a t -> 'a
+end
+
+(** {1 Lexbuf interface} *)
+
+val parse_lexbuf
+  :  (module S with type t = 'a)
+  -> path:Fpath.t
+  -> lexbuf:Lexing.lexbuf
+  -> 'a Parsing_result.t
+
+val parse_lexbuf_exn
+  :  (module S with type t = 'a)
+  -> path:Fpath.t
+  -> lexbuf:Lexing.lexbuf
+  -> 'a
+
+(** {1 File interface} *)
+
+val parse_file : (module S with type t = 'a) -> path:Fpath.t -> 'a Parsing_result.t
+val parse_file_exn : (module S with type t = 'a) -> path:Fpath.t -> 'a
